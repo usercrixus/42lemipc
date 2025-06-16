cacdeff137b72293508e9545005b512fb65d3e93
Adrien Chaisneau <achaisne@f1r8s11.clusters.42paris.fr>
2025-06-16 16:53:45 +0200

    end game function added

--- srcs/game.c
+++ srcs/game.c
@@ -132,7 +132,8 @@ t_move getBestMove()
 
 	// === LEADER SELECTS A TARGET ===
 	int isLeader = 1;
-	for (int i = 0; i < MAX_PLAYER; i++) {
+	for (int i = 0; i < MAX_PLAYER; i++)
+	{
 		t_player *p = &shared->players[i];
 		if (p->isAlive && p->symbole == self->symbole && p->playerId < self->playerId)
 			isLeader = 0;
@@ -141,22 +142,26 @@ t_move getBestMove()
 	int msgid = msgget(MSGQ_KEY, 0666 | IPC_CREAT);
 	t_msg_target msg;
 
-	if (isLeader) {
+	if (isLeader)
+	{
 		int minDist = 9999;
 		t_player *closest = NULL;
 
-		for (int i = 0; i < MAX_PLAYER; i++) {
+		for (int i = 0; i < MAX_PLAYER; i++)
+		{
 			t_player *enemy = &shared->players[i];
 			if (!enemy->isAlive || enemy->symbole == self->symbole)
 				continue;
 			int dist = abs(enemy->x - self->x) + abs(enemy->y - self->y);
-			if (dist < minDist) {
+			if (dist < minDist)
+			{
 				minDist = dist;
 				closest = enemy;
 			}
 		}
 
-		if (closest) {
+		if (closest)
+		{
 			msg.mtype = MSG_TYPE_TARGET;
 			msg.targetX = closest->x;
 			msg.targetY = closest->y;
@@ -165,9 +170,13 @@ t_move getBestMove()
 			tx = closest->x;
 			ty = closest->y;
 		}
-	} else {
-		while (msgrcv(msgid, &msg, sizeof(t_msg_target) - sizeof(long), MSG_TYPE_TARGET, IPC_NOWAIT) > 0) {
-			if (msg.team == self->symbole) {
+	}
+	else
+	{
+		while (msgrcv(msgid, &msg, sizeof(t_msg_target) - sizeof(long), MSG_TYPE_TARGET, IPC_NOWAIT) > 0)
+		{
+			if (msg.team == self->symbole)
+			{
 				tx = msg.targetX;
 				ty = msg.targetY;
 			}
@@ -181,7 +190,8 @@ t_move getBestMove()
 	int dx = tx - self->x;
 	int dy = ty - self->y;
 
-	typedef struct {
+	typedef struct
+	{
 		t_move dir;
 		int offsetX;
 		int offsetY;
@@ -191,14 +201,17 @@ t_move getBestMove()
 	int count = 0;
 
 	// Prefer axis with greater distance
-	if (abs(dx) >= abs(dy)) {
-		moveOptions[count++] = (MoveOption){ dx > 0 ? RIGHT : LEFT, dx > 0 ? 1 : -1, 0 };
+	if (abs(dx) >= abs(dy))
+	{
+		moveOptions[count++] = (MoveOption){dx > 0 ? RIGHT : LEFT, dx > 0 ? 1 : -1, 0};
 		if (dy != 0)
-			moveOptions[count++] = (MoveOption){ dy > 0 ? BOT : TOP, 0, dy > 0 ? 1 : -1 };
-	} else {
-		moveOptions[count++] = (MoveOption){ dy > 0 ? BOT : TOP, 0, dy > 0 ? 1 : -1 };
+			moveOptions[count++] = (MoveOption){dy > 0 ? BOT : TOP, 0, dy > 0 ? 1 : -1};
+	}
+	else
+	{
+		moveOptions[count++] = (MoveOption){dy > 0 ? BOT : TOP, 0, dy > 0 ? 1 : -1};
 		if (dx != 0)
-			moveOptions[count++] = (MoveOption){ dx > 0 ? RIGHT : LEFT, dx > 0 ? 1 : -1, 0 };
+			moveOptions[count++] = (MoveOption){dx > 0 ? RIGHT : LEFT, dx > 0 ? 1 : -1, 0};
 	}
 
 	// Add remaining directions to complete fallback
@@ -206,21 +219,26 @@ t_move getBestMove()
 	int dxs[4] = {0, 0, -1, 1};
 	int dys[4] = {-1, 1, 0, 0};
 
-	for (int i = 0; i < 4; i++) {
+	for (int i = 0; i < 4; i++)
+	{
 		int alreadyAdded = 0;
-		for (int j = 0; j < count; j++) {
-			if (moveOptions[j].dir == allDirs[i]) {
+		for (int j = 0; j < count; j++)
+		{
+			if (moveOptions[j].dir == allDirs[i])
+			{
 				alreadyAdded = 1;
 				break;
 			}
 		}
-		if (!alreadyAdded && count < 4) {
-			moveOptions[count++] = (MoveOption){ allDirs[i], dxs[i], dys[i] };
+		if (!alreadyAdded && count < 4)
+		{
+			moveOptions[count++] = (MoveOption){allDirs[i], dxs[i], dys[i]};
 		}
 	}
 
 	// Try each move in order of preference
-	for (int i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++)
+	{
 		int newX = self->x + moveOptions[i].offsetX;
 		int newY = self->y + moveOptions[i].offsetY;
 
@@ -233,4 +251,25 @@ t_move getBestMove()
 	}
 
 	return STAY;
+}
+
+bool isGameEnd()
+{
+	char team;
+
+	team = '0';
+	for (size_t y = 0; y < MAP_HEIGHT; y++)
+	{
+		for (size_t x = 0; x < MAP_WIDTH; x++)
+		{
+			if (shared->map[y][x] != team)
+			{
+				if (team == '0')
+					team = shared->map[y][x];
+				else
+					return (true);
+			}
+		}
+	}
+	return (false);
 }
